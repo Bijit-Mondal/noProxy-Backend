c35519c282e28bbe2dbfa99bc182f80592e29b95
Bijit-Mondal <bijitmondal3011@rediffmail.com>
2023-02-22 10:23:21 +0530

    first commit

---
+++ src/main/java/website/ilib/noproxy/repository/StudentRepository.java
@@ -0,0 +1,19 @@
+package website.ilib.noproxy.repository;
+
+import website.ilib.noproxy.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+
+    public boolean existsByEmail(String email);
+
+    public List<Student> findByEmail(String email);
+
+    @Query("select max(s.id) from Student s")
+    public Integer findMaxId();
+}
---
+++ src/test/java/website/ilib/noproxy/SpringBootAndSqLiteApplicationTests.java
@@ -0,0 +1,13 @@
+package website.ilib.noproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAndSqLiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
